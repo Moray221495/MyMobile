b6920146a80bf8a3a5f1ca549cdf24d2a38e483a
Moray221495 <moray221495@gmail.com>
2016-08-31 12:46:12 +0200

    Contact fomular

--- app/src/main/java/mymobile/mymobile/NewsActivity.java
+++ app/src/main/java/mymobile/mymobile/NewsActivity.java
@@ -2,6 +2,7 @@ package mymobile.mymobile;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
 /**
  * Created by jange on 28.08.2016.
@@ -12,6 +13,9 @@ public class NewsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
 
+        WebView webview = new WebView(this);
+        setContentView(webview);
 
+        webview.loadUrl("http://dev.morayinteractivestudios.com/mymobile/redirect.php/");
     }
 }
